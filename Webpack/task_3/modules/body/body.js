900fbfdd73956bcca0afcc71f7fd43d4cabbf8db
Adel Knode <ann.knode@atlasschool.com>
2024-05-07 18:12:50 +0000

    nearly

--- Webpack/task_3/modules/body/body.js
+++ Webpack/task_3/modules/body/body.js
@@ -1,6 +1,17 @@
-
-
-
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css';
 
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+
+$('button').on('click', debouncedUpdateCounter);
--- Webpack/task_3/modules/header/header.js
+++ Webpack/task_3/modules/header/header.js
@@ -1,5 +1,10 @@
 import $ from 'jquery';
-import 'holberton_logo.jpg' from './assets/holberton_logo.jpg'; 
+import './header.css';
+import holbertonLogo from '../../assets/holberton_logo.jpg';
+
+
 
 $('body').append('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
+
+console.log('Init header')
