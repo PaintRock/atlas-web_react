7fee603f48af5f4da3b883a6576e92c8f7d6d2b6
Adel Knode <ann.knode@atlasschool.com>
2024-05-09 05:09:47 +0000

    all

---
+++ react_props/task_1/dashboard/config/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192, // inline files smaller than 8 KB as data URLs
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+        }),
+    ],
+    devtool: 'inline-source-map',
+};
---
+++ react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem';
+
+export default function Notifications() {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+  return (
+    <div className="Notifications">
+      <button
+        style={{
+          position: 'absolute',
+          right: '20px',
+          top: '20px',
+          background: 'none',
+          border: 'none',
+          cursor: 'pointer',
+          outline: 'none',
+        }}
+        aria-label="Close"
+        onClick={handleClick}
+      >
+        <span style={{ fontSize: '30px' }}>&times;</span>
+      </button>
+      <p>Here is the list of notifications</p>
+      <p>Here is the list of notifications</p>
+    <ul>
+      <NotificationItem type="default" value="New course available" />
+      <NotificationItem type="urgent" value="New resume available" />
+      <NotificationItem
+        type="urgent"
+        html={{ __html: getLatestNotification() }}
+      />
+    </ul>
+    </div>
+  );
+}
