566a027f789b954e80df83af0a4cc788d37a1a33
Adel Knode <ann.knode@atlasschool.com>
2024-05-14 06:15:33 +0000

    many installs

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './App/App.js';
+import Notifications from './Notifications/Notifications.js';
+import './App/App.css';
+import './Notifications/Notifications.css';
 
-console.log('Hello, Webpack!');
+const h1 = document.createElement('h1');
+h1.innerHTML = "Maybe today is THAT DAY";
+document.body.appendChild(h1);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>
+);
